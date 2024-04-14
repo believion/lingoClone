03cd0688374eaf5e110b6e8488445f3beefa539e
tanish arora <tanisharora2312@gmail.com>
2024-04-15 00:40:41 +0530

    Added Footer and footer code

--- app/lesson/footer.tsx
+++ app/lesson/footer.tsx
@@ -1,6 +1,62 @@
-const Footer = () => {
+import {useKey, useMedia} from "react-use";
+import {CheckCircle, XCircle} from "lucide-react";
+
+import {cn} from "@/lib/utils";
+import {Button} from "@/components/ui/button";
+
+type Props = {
+    status: "correct" | "wrong" | "none" | "completed";
+    onCheck: () => void;
+    disabled?: boolean;
+    lessonId?: boolean; // don't know why this is boolean
+}
+
+const Footer = ({
+                    disabled,
+                    status,
+                    onCheck,
+                    lessonId
+                }: Props) => {
+    useKey("Enter", onCheck, {}, [onCheck]);
+    const isMobile = useMedia("(max-width: 1024px)"); // when width is greater than this, isMobile will become false
+
     return (
-        <div>Footer</div>
+        <footer className={cn("lg:h-[140px] h-[100px] border-t-2",
+            status === "correct" && "border-transparent bg-green-100",
+            status === "wrong" && "border-transparent bg-rose-100"
+        )}>
+            <div className="max-w-[1140px] h-full mx-auto flex items-center justify-between px-6 lg:px-10">
+                {status === "correct" && (
+                    <div className="text-green-500 font-bold text-base lg:text-2xl flex items-center">
+                        <CheckCircle className="h-6 w-6 lg:h-10 lg:w-10 mr-4"/>
+                        Nicely Done !
+                    </div>
+                )}
+                {status === "wrong" && (
+                    <div className="text-rose-500 font-bold text-base lg:text-2xl flex items-center">
+                        <XCircle className="h-6 w-6 lg:h-10 lg:w-10 mr-4"/>
+                        Try Again !
+                    </div>
+                )}
+                {status === "completed" && (
+                    <Button
+                        variant="default"
+                        size={isMobile ? "sm" : "lg"}
+                        onClick={() => window.location.href = `/lesson/${lessonId}`}>
+                        Practice Again
+                    </Button>
+                )}
+                <Button disabled={disabled} className="ml-auto" onClick={onCheck}
+                        size={isMobile ? "sm" : "lg"}
+                        variant={status === "wrong" ? "danger" : "secondary"}
+                >
+                    {status === "none" && "Check"}
+                    {status === "wrong" && "Retry"}
+                    {status === "correct" && "Next"}
+                    {status === "completed" && "Continue"}
+                </Button>
+            </div>
+        </footer>
     )
 }
 
